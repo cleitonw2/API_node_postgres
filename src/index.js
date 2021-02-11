5bcf656b69e88f06c6fb6b3e42f9767e37a82118
Cleiton <cleitonwoycik@outlook.com>
2021-02-11 11:28:41 -0300

    registering first user in the database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const { User } = require('./app/models');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json('Hello World!');
+app.post('/', (req, res) => {
+    const { name, email, password } = req.body;
+    User.create({ name: name, email: email, password: password });
+    res.json('usuário cadastrado com sucesso');
 });
 
 app.listen(3000);
