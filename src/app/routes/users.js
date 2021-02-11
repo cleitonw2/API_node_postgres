7c725b7ff6c646ee8c703f89e456761b64509cbe
Cleiton <cleitonwoycik@outlook.com>
2021-02-11 17:19:54 -0300

    turning the password into hash

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -11,8 +11,28 @@ router.post('/register', async (req, res) => {
         const user = await User.create({ name: name, email: email, password: passwordHash });
         res.json(user);
     } catch (error) {
-        console.log(error)
         res.status(500).json({ error: 'Error registering new user please try again.' })
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        const user = await User.findOne({ where: { email: email } });
+        if(!user)
+            res.status(500).json('user not found');
+        if (user) {
+            const validPassword = await bcrypt.compare(password, user.password);
+            if (validPassword) {
+                user.password = undefined;
+                res.status(200).json(user);
+            } else {
+                res.status(500).json({ error: 'invalid password' });
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error });
     }
 });
 
